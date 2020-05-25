cc644cee36bd9fab8fb3b22c40e2e8694101884e
zhangqi <zhangqi@qq.com>
2020-05-25 21:53:08 +0800

    123

--- vuehr/src/views/sys/SysLog.vue
+++ vuehr/src/views/sys/SysLog.vue
@@ -26,6 +26,7 @@
             </el-table>
         </div>
 
+<!--        分页查询-->
         <div style="display: flex;justify-content: flex-end">
             <el-pagination
                     background
@@ -35,7 +36,7 @@
                     :total="total">
             </el-pagination>
         </div>
-
+        <!--        分页查询-->
     </div>
 </template>
 
@@ -49,27 +50,37 @@
                 search: {
                     size: 10,
                     page: 1,
+                    //关键字
                     keyword: null
                 },
                 tableData: [],
                 total: null,
             }
-        }, created() {
+        },
+        //vue的钩子函数,当组件创建的时候会执行created()里的方法
+        created() {
             this.getList()
         },
         methods: {
+
             getList() {
                 postRequest('/system/log/list', this.search).then(res => {
                     this.tableData = res.data
                     this.total = res.total
                 })
             },
-            sizeChange(currentSize) {
+            //当分页大小变化时触发的回调函数
+            sizeChange(currentSize) { //currentSize 改变的值 （页面大小的值  每页有10，20)
+                //将页面的大小变更为当前值
                 this.search.size = currentSize;
+                //用当前的数据进行查询
                 this.getList();
             },
-            currentChange(currentPage) {
+            //当页码发生变化时触发的回调函数
+            currentChange(currentPage) { //currentPage 改变的值  (页码的值  第1，2页）
+                //将页码变更为当前值
                 this.search.page = currentPage;
+                //用当前的数据进行查询
                 this.getList();
             }
         }
